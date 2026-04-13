672dee0084357c75fcfa9d4cbff871ce0446449f
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-13 14:13:07 -0300

    Add files via upload

---
+++ Area quadrado-Aresta/projeto2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Valor da Aresta do Quadrado");
+            Double valorAresta = Double.Parse(Console.ReadLine());
+
+           Console.WriteLine(valorAresta
+                + " é o valor da Aresta do Quadrado");
+           // Console.WriteLine("O Perímetro do Quadrado é: " + (valorAresta * 4));
+           //
+           Console.WriteLine("A Área do Quadrado é: " + (valorAresta * valorAresta));      
+                
+
+
+        }
+    }
+}
