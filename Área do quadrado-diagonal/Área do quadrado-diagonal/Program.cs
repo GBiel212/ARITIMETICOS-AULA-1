047a1d2212fba15ec62918c9518013a2b0cfe95d
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-13 14:05:53 -0300

    Add files via upload

---
+++ Área do quadrado-diagonal/Área do quadrado-diagonal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_do_quadrado_diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a diagonal do quadrado:");
+            double diagonal = Convert.ToDouble(Console.ReadLine());
+            double area = (diagonal * diagonal) / 2;
+            Console.WriteLine("A área do quadrado é: " + area); 
+        }
+    }
+}
