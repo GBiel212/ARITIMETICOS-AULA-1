2c1dc06211377c9d71225ec54e76d04e00474fe7
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-13 14:03:48 -0300

    Add files via upload

---
+++ AREA CIRCULO/AREA CIRCULO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AREA_CIRCULO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o Diametro do Circulo: ");
+            double diametro = Convert.ToDouble(Console.ReadLine());
+            double raio = diametro / 2;
+            double area = Math.PI * Math.Pow(raio, 2);
+            Console.WriteLine("A area do circulo é: " + area);
+
+            Console.Write(" Obrigado! ");
+            Console.WriteLine(" Pressione qualquer tecla para sair... ");
+            Console.ReadKey();  
+
+
+
+        }
+
+
+
+        
+    }
+}
